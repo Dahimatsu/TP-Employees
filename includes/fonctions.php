ef2c6ca3ee4f9fc29a65c79d09f325d2b2ffd55b
RAVELOMANANTSOA Tony Mahefa <rtonymahefa@gmail.com>
2025-06-23 10:55:49 +0300

    v0.3

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -1,1 +1,17 @@
-<?php ?>
+<?php
+require('includes/connexion.php');
+
+function getAllDepartments()
+{
+    $connect = dbconnect();
+    $query = "SELECT * FROM departments";
+    $result = mysqli_query($connect, $query);
+
+    $departments = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $departments[] = $row;
+    }
+
+    mysqli_free_result($result);
+    return $departments;
+}
---
+++ pages/accueil.php
@@ -0,0 +1,7 @@
+<?php
+$departements = getAllDepartments();
+?>
+<section>
+    
+</section>
+
